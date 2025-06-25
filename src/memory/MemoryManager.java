21949195a0726968517774f56248f08ce44e5221
TheGreatWizard16 <horladimeji10@gmail.com>
2025-06-25 16:50:11 +0200

    Implemented casio  scientific calculator UI and logic

--- src/memory/MemoryManager.java
+++ src/memory/MemoryManager.java
@@ -1,5 +1,25 @@
 package memory;
 
 public class MemoryManager {
+    private double memoryValue = 0.0;
 
+    public void store(double value) {
+        memoryValue = value;
+    }
+
+    public double recall() {
+        return memoryValue;
+    }
+
+    public void clear() {
+        memoryValue = 0.0;
+    }
+
+    public void add(double value) {
+        memoryValue += value;
+    }
+
+    public void subtract(double value) {
+        memoryValue -= value;
+    }
 }
